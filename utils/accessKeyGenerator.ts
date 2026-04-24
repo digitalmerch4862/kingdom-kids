e57567ab93264a08062f88a9f2c1e0226041ac91
DigitalMerch <digitalmerch4862@gmail.com>
2026-04-24 15:28:13 +0800

    test: accessKeyGenerator failing tests

---
+++ utils/accessKeyGenerator.ts
@@ -0,0 +1,7 @@
+export function generateAccessKey(year: number, existingKeys: string[]): string {
+  throw new Error('not implemented');
+}
+
+export function generateBatchAccessKeys(year: number, existingKeys: string[], count: number): string[] {
+  throw new Error('not implemented');
+}
