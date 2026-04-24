0d9baeada9946ecd0a06c8f848190583b708ff12
DigitalMerch <digitalmerch4862@gmail.com>
2026-04-25 00:47:33 +0800

    test: sundayStreak failing tests

---
+++ utils/sundayStreak.ts
@@ -0,0 +1,15 @@
+export function getLastNSundays(from: Date, n: number): string[] {
+  throw new Error('not implemented');
+}
+
+export function computeConsecutiveStreak(
+  attendedDates: string[],
+  from: Date,
+  maxStreak: number = 4
+): number {
+  throw new Error('not implemented');
+}
+
+export function sundaysInMonth(year: number, month: number): string[] {
+  throw new Error('not implemented');
+}
